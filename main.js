3ba4870c96443ea25955c314a0060a417e677fe8
Tripti Sharma <106910707+Tripti05@users.noreply.github.com>
2022-07-19 16:59:45 +0530

    Add basic dashboard files

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ dropdowns.forEach(dropdown => {
     downBtn.classList.toggle("rotate__btn");
   });
 });
+
+
+// handle search input
+var searchInput = document.querySelector('#searchInput');
+searchInput.onkeydown = function(e){
+
+  if(e.keyCode == 13){
+    let value = searchInput.value;
+    let target = document.querySelector('search-result');
+    // search api call
+    let url = `https://tva.staging.b2brain.com/search/autocomplete_org_all/?q=${value}`
+    fetch(url,{
+      method: 'GET'
+    })
+    .then(res=>res.json())
+    .then(data=>{
+      // data
+
+    })
+  }
+}
